4b4bd0580052846a8ae63d4e68e776e563d99d6a
appyshah08 <appflamaboy@gmail.com>
2017-02-05 14:00:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dhaval/assignment2b/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.dhaval.assignment2b;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText1;
+    EditText editText2;
+    EditText editText3;
+    EditText editText4;
+    EditText editText5;
+    EditText editText6;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
